5f1ac03b64fda5b36e5834414901a85980bc7a1f
StephanieSST <stephaniesteuernagel@gmail.com>
2024-04-16 23:48:38 -0300

    crud

--- blogpessoal/src/postagem/services/postagem.service.ts
+++ blogpessoal/src/postagem/services/postagem.service.ts
@@ -1,10 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
-
-
-
 
 
 @Injectable()
@@ -16,5 +13,51 @@ export class PostagemService {
 
     async findAll(): Promise<Postagem[]> {
         return await this.postagemRepository.find();
+    }
+
+    async findById(id: number): Promise<Postagem> {
+        let postagem = await this.postagemRepository.findOne({
+            where:{
+                id
+            }
+        });
+        if(!postagem)
+            throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+
+        //throw new HttpException(postagem, HttpStatus.OK)
+       
+        return postagem;
+    }
+
+    async findByTitulo(titulo: string): Promise<Postagem[]>{
+        return await this.postagemRepository.find({
+            where: {
+                titulo: ILike(`%${titulo}%`)
+            }
+        })
+
+    }
+
+    async create(postagem: Postagem): Promise<Postagem> {
+        return await this.postagemRepository.save(postagem)
+    }
+
+    async update(postagem: Postagem): Promise<Postagem>{
+        let buscaPostagem: Postagem = await this.findById(postagem.id);
+
+        if (!buscaPostagem || !postagem.id)
+            throw new HttpException("Postagem não encontrada!", HttpStatus.NOT_FOUND);
+
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async delete(id: number): Promise<DeleteResult>{
+
+        let buscaPostagem: Postagem = await this.findById(id);
+
+        if(buscaPostagem)
+            throw new HttpException('Postagem não foi encontrada!', HttpStatus.NOT_FOUND)
+        
+        return await this.postagemRepository.delete(id);
     }
 }
